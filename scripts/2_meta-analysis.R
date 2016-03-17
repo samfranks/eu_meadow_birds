bcc9760d899907669588e0764f270f05559488c1
sam.franks@bto.org <sam.franks@bto.org>
2016-03-17 09:48:57 +0000

    HD: worked on convergence issues for success models
    Identified some problems due to their being no 0's or no 1's in some
    categories when breaking down to the species level. Ben Bolker had some
    useful advice on Stackoverflow.

--- scripts/2_meta-analysis.R
+++ scripts/2_meta-analysis.R
@@ -14,7 +14,7 @@
 
 #=================================  LOAD PACKAGES =================================
 
-list.of.packages <- c("MASS","reshape","raster","sp","rgeos","rgdal","lme4")
+list.of.packages <- c("MASS","reshape","raster","sp","rgeos","rgdal","lme4","car","blme")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
@@ -82,6 +82,10 @@ for (i in 1:length(mgmtvars)) {
 
 #=================================  ANALYSIS  ===============================
 
+# Analysis evaluates whether one or several management interventions has a significantly positive outcome (i.e. is successful) compared to the baseline reference situation (which in most cases is no management)
+# reference level for all interventions is 'none', so management (either applied or reduced, depending on the intervention) is evaluated against the reference
+# are studies which don't apply a particular management intervention more likely to have a positive outcome for the metric being measured than studies that don't apply that intervention? 
+
 # 0a) success of individual management types overall
 # 0b) success of individual management types by species
 # 1a) success of AES and nature reserves
@@ -103,31 +107,112 @@ for (i in 1:length(mgmtvars)) {
 
 #------------------------------ 0a) success of individual management types -------------------------
 
-m <- list()
+# identify which categories have low numbers
+out <- list()
+for(i in 1:length(mgmtvars)) {
+  out[[i]] <- table(dat$species, dat[,mgmtvars[i]])
+}
+names(out) <- mgmtvars
+out
 
-print(Sys.time())
+# subset out categories with few observations
+mdat <- subset(dat, mowing!="applied" & fertpest!="applied" & predator.control!="reduced")
+mdat <- droplevels(mdat)
+
+m.ind <- list()
 
 for (i in 1:length(mgmtvars)) {
   
-#   mdat <- dat[dat[,mgmtvars[i]]!="none",]
-#   mdat <- droplevels(mdat)
-  
-  mdat <- dat
-  
   modform <- as.formula(paste("success ~ ", mgmtvars[i], " + study.length + analysis2 + sample.size + (1|reference) + (1|country)", sep=""))
-  m[[i]] <- glmer(modform, data=mdat, family=binomial, control=glmerControl(optimizer="bobyqa"))
+  m.ind[[i]] <- glmer(modform, data=mdat, family=binomial, control=glmerControl(optimizer="bobyqa"))
   
 }
 
+warningmessages <- lapply(m.ind, function(x) slot(x, "optinfo")$conv$lme4$messages)
+warningmessages
+
+### Output model results ###
+
+setwd(outputwd)
+sink(paste("abundance_0a.txt", sep=" "))
+cat("\n########==========  0a) success of individual management types ==========########\n", sep="\n")
+print(lapply(m.ind, summary))
+print(warningmessages)
+sink()
+
+#------------------------------ 0b) success of individual management types by species -------------------------
+
+# subset dataset to remove dunlin and ruff, not enough data for these species for any analysis that includes species as a covariate
+mdat <- subset(mdat, species!="dunlin" & species!="ruff")
+mdat <- droplevels(mdat)
+
+# identify which categories have low numbers
+out <- list()
+for(i in 1:length(mgmtvars)) {
+  out[[i]] <- table(mdat$species, mdat[,mgmtvars[i]])
+}
+names(out) <- mgmtvars
+out
+
+# problems with all 0's or all 1's in a single category level - complete separation
+# try Ben Bolker's fix using the blme package
+# Your intuition is exactly right. This phenomenon is called complete separation. You can find quite a lot (now that you know its name) Googling around ... It is fairly thoroughly discussed here in a general context, and here in the context of GLMMs. The standard solution to this problem is to add a small term that pushes the parameters back toward zero -- in frequentist contexts this is called a penalized or bias-corrected method. The standard algorithm is due to Firth (1993, "Bias reduction of maximum likelihood estimates" Biometrika 80, 27-38), and is implemented in the logistf package on CRAN. In Bayesian contexts this is framed as adding a weak prior to the fixed-effect parameters.
+# 
+# To my knowledge Firth's algorithm hasn't been extended to GLMMs, but you can use the Bayesian trick by using the blme package, which puts a thin Bayesian layer over the top of the lme4 package. Here's an example from the above-linked GLMM discussion:
+# cmod_blme_L2 <- bglmer(predation~ttt+(1|block),data=newdat,
+# family=binomial,
+# fixef.prior = normal(cov = diag(9,4)))
+# The first two lines in this example are exactly the same as we would use in the standard glmer model; the last specifies that the prior for the fixed effects is a multivariate normal distribution with a diagonal variance-covariance matrix. The matrix is 4x4 (because we have 4 fixed-effect parameters in this example), and the prior variance of each parameter is 9 (corresponding to a standard deviation of 3, which is pretty weak -- that means +/- 2SD is (-6,6), which is a very large range on the logit scale).
+# 
+# The very large standard errors of the parameters in your example are an example of a phenomenon closely related to complete separation (it occurs whenever we get extreme parameter values in a logistic model) called the Hauck-Donner effect.
+
+m.ind.sp <- list()
+
+for (i in 1:length(mgmtvars)) {
+  
+#   # remove additional combinations/categories of interactions/species for which there aren't enough data
+#   if (mgmtvars[i]=="mowing") {
+#     mdat <- subset(mdat, mowing!="applied")
+#     mdat <- droplevels(mdat)
+#   }
+#   
+#   if (mgmtvars[i]=="grazing") {
+#     mdat <- subset(mdat, mowing!="applied")
+#     mdat <- droplevels(mdat)
+#   }
+  
+  
+  modform <- as.formula(paste("success ~ ", mgmtvars[i], "*species + study.length + analysis2 + sample.size + (1|reference) + (1|country)", sep=""))
+  m.ind.sp[[i]] <- glmer(modform, data=mdat, family=binomial, control=glmerControl(optimizer="bobyqa"))
+  
+}
+
+warningmessages <- lapply(m.ind.sp, function(x) slot(x, "optinfo")$conv$lme4$messages)
+warningmessages
+
+mdat <- subset(dat, species!="dunlin" & species!="ruff" & mowing!="applied")
+mdat <- droplevels(mdat)
+
+m.test <- glmer(success ~ AE.level*species + study.length + analysis2 + sample.size + (1|country), data=mdat, family=binomial)
+summary(m.test)
+slot(m.test, "optinfo")$conv$lme4$messages
+
+tt <- getME(m.test,"theta")
+ll <- getME(m.test,"lower")
+min(tt[ll==0])
+
 print(Sys.time())
+
+setwd(outputwd)
+sink(paste("abundance_0b.txt", sep=" "))
+cat("\n########==========  0b) success of individual management types by species ==========########\n", sep="\n")
+print(lapply(m.ind.sp, summary))
+sink()
 
 
 #------------------------------ 1a) overall success of AES and nature reserves -------------------------
-m <- list()
 
-# subset to include data where AES or reserve management was attempted
-subdat <- subset(dat, AE.level!="none" | reserve.desig!="none")
-subdat <- droplevels(subdat)
+m <- list()
 
 print(Sys.time())
 
@@ -145,26 +230,27 @@ print(Sys.time())
 
 #------------------------------ 2a) success of AES and nature reserves by species -------------------------
 
-# subset dataset to remove dunline and ruff, not enough data for these species
-subdat <- subset(dat, species!="dunlin" & species!="ruff")
-subdat <- droplevels(subdat)
+# subset dataset to remove dunlin and ruff, not enough data for these species
+mdat <- subset(dat, species!="dunlin" & species!="ruff")
+mdat <- droplevels(mdat)
+
 
 print(Sys.time())
 
-m[[3]] <- glmer(success ~ AE.level*species + reserve.desig*species + study.length + analysis2 + sample.size + (1|reference) + (1|country), data=subdat, family=binomial, control=glmerControl(optimizer="bobyqa"))
+m[[3]] <- glmer(success ~ AE.level*species + reserve.desig*species + study.length + analysis2 + sample.size + (1|reference) + (1|country), data=mdat, family=binomial, control=glmerControl(optimizer="bobyqa"))
 
 print(Sys.time())
 
 
 #------------------------------ 2b) success of specific management interventions by species -------------------------
 
-# subset dataset to remove dunline and ruff, not enough data for these species
-subdat <- subset(dat, species!="dunlin" & species!="ruff")
-subdat <- droplevels(subdat)
+# subset dataset to remove dunlin and ruff, not enough data for these species
+mdat <- subset(dat, species!="dunlin" & species!="ruff")
+mdat <- droplevels(mdat)
 
 print(Sys.time())
 
-m[[3]] <- glmer(success ~ AE.level*species + reserve.desig*species + study.length + analysis2 + sample.size + (1|reference) + (1|country), data=subdat, family=binomial, control=glmerControl(optimizer="bobyqa"))
+m[[4]] <- glmer(success ~ mowing*species + grazing*species + fertpest*species + nest.protect*species + predator.control*species + water*species + study.length + analysis2 + sample.size + (1|reference) + (1|country), data=mdat, family=binomial, control=glmerControl(optimizer="bobyqa"))
 
 print(Sys.time())
 
